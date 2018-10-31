ae9ad8dd1f47f2efa8831e71a2babf45815d53fa
aofleejay <aofleejay@hotmail.com>
2018-10-31 18:21:18 +0700

    Try hooks

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,10 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Layout from './common/Layout'
 
-const Profile = React.memo(() => (
-  <Layout>
-    <h1>Profile</h1>
-  </Layout>
-))
+const Profile = React.memo(() => {
+  const [counter, setCounter] = useState(0)
+
+  const decrease = () => {
+    setCounter(counter - 1)
+  }
+
+  const increase = () => {
+    setCounter(counter + 1)
+  }
+
+  return (
+    <Layout>
+      <button type="button" onClick={decrease}>-</button>
+      <p>{counter}</p>
+      <button type="button" onClick={increase}>+</button>
+    </Layout>
+  )
+})
 
 export default Profile
